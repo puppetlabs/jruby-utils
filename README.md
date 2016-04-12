93e209928b8dfe2e0147a58e774d5c69ccfdfc52
Chris Price <chris@puppetlabs.com>
2016-04-12 12:34:34 -0700

    (SERVER-1162) Bring back jruby-utils project file, readmes

--- README.md
+++ README.md
@@ -1,98 +1,15 @@
-# Puppet Server
+# JRuby Utils
 
-Puppet Server is the next-generation application for managing Puppet agents.
-This platform will carry Puppet's server-side components to a more
-distributed, service-oriented architecture. We've built Puppet Server on top of the
-same technologies that have made PuppetDB successful, allowing us to
-greatly improve performance, scalability, advanced metrics collection,
-and fine-grained control over the Ruby runtime.
+A library for interacting with JRuby instances from Clojure.
 
-While Puppet Server is meant to replace an existing
-Apache/Passenger Puppet master stack, there are a handful of differences
-due to changes in the underlying architecture. Please see [Puppet Server vs. Apache/Passenger Puppet Master](./documentation/puppetserver_vs_passenger.markdown) for details.
-
-## Installing Puppet Server
-
-Puppet Server depends on Puppet 3.7.3 or later, so if you install it on a system running an older version of Puppet, installation will also upgrade Puppet itself. Please see [Installing Puppet Server from Packages](./documentation/install_from_packages.markdown) for complete installation  requirements and instructions.
-
-## Ruby and Puppet Server
-
-Puppet Server is compatible with Ruby 1.9. If you are installing
-Puppet Server on an existing system with Ruby 1.8, the behavior of some extensions, such as custom functions and custom resource types and providers, might change slightly. Generally speaking, this shouldn't affect core Puppet Ruby code, which is tested against both versions of Ruby.
-
-Puppet Server uses its own JRuby interpreter, which doesn't load gems or other code from your system Ruby. If you want Puppet Server to load any additional gems, use the Puppet Server-specific `gem` command to install them. See [Puppet Server and Gems](./documentation/gems.markdown) for more information about gems and Puppet Server.
-
-## Configuration
-
-Puppet Server honors almost all settings in `puppet.conf` and should pick them
-up automatically. However, we have also introduced some new Puppet
-Server-specific settings---please see the [Configuration]
-(./documentation/configuration.markdown) page for details. For more information
-on the specific differences in Puppet Server's support for `puppet.conf`
-settings as compared to the Ruby master, see the [puppet.conf differences]
-(./documentation/puppet_conf_setting_diffs.markdown) page.
-
-### Certificate Authority Configuration
-
-Much of the existing documentation on [External CA Support for the Ruby Puppet Master](https://docs.puppetlabs.com/puppet/latest/reference/config_ssl_external_ca.html)
-still applies to using an external Certificate Authority in conjunction with Puppet Server. There are some  differences to bear in mind, however; see the [External CA Configuration](./documentation/external_ca_configuration.markdown) page for details.
-
-### SSL Configuration
-
-In network configurations that require external SSL termination, you need to do a few things differently in Puppet Server. Please see the [External SSL Termination](./documentation/external_ssl_termination.markdown) page for details.
-
-## CLI Commands
-
-Puppet Server provides several command-line utilities useful for development
-and debugging purposes. These commands are all aware of `puppet-server.conf`
-and the gems and Ruby code specific to Puppet Server and Puppet, while keeping
-them isolated from your system Ruby.
-
-For more information, see [Puppet Server Subcommands](./documentation/subcommands.markdown).
-
-## Known Issues
-
-As this application is still in development, there are a few [known issues](./documentation/known_issues.markdown) that you should be aware of.
-
-## Developer Documentation
-
-If you are a developer who wants to play with our code, these documents should prove useful:
-* [Running Puppet Server From Source](./documentation/dev_running_from_source.markdown)
-* [Debugging](./documentation/dev_debugging.markdown)
-* [Puppet Server Subcommands](./documentation/subcommands.markdown)
-
-## Branching Strategy
-
-The Branching Strategy for Puppet Server is documented on the wiki [here](https://github.com/puppetlabs/puppet-server/wiki/Branching-Strategy).
-
-## Issue Tracker
-
-Feature requests?  Found a bug?  Want to see what issues are currently in flight?  Please visit our [Jira project](https://tickets.puppetlabs.com/browse/SERVER).
+[![Build Status](https://travis-ci.org/puppetlabs/jruby-utils.svg)](https://travis-ci.org/puppetlabs/jruby-utils)
 
 ## License
 
-Copyright © 2013 -- 2014 Puppet Labs
+See [LICENSE](LICENSE).
 
-Distributed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+## Support
 
-### Special thanks to
-
-#### Cursive Clojure
-
-[Cursive](https://cursiveclojure.com/) is a Clojure IDE based on
-[IntelliJ IDEA](http://www.jetbrains.com/idea/download/index.html).  Several of
-us at Puppet Labs use it regularly and couldn't live without it.  It's got
-some really great editing, refactoring, and debugging features, and the author,
-Colin Fleming, has been amazingly helpful and responsive when we have feedback.
-If you're a Clojure developer you should definitely check it out!
-
-#### JRuby
-
-[JRuby](http://jruby.org/) is an implementation of the Ruby programming language
-that runs on the JVM.  It's a fantastic project, and it is the bridge that allows
-us to run all of the existing Puppet Ruby code while taking advantage of all of
-the advanced features and libraries that are available on the JVM.  We're very
-grateful to the developers for building such a great product and for helping us
-work through a few bugs that we've discovered along the way.
-
-[leiningen]: https://github.com/technomancy/leiningen
+We use the [Trapperkeeper project on
+JIRA](https://tickets.puppetlabs.com/browse/TK) for tickets on this project,
+although Github issues are welcome too.
--- project.clj
+++ project.clj
@@ -1,43 +1,27 @@
-(def clj-version "1.7.0")
-(def tk-version "1.3.1")
-(def tk-jetty-version "1.5.5")
 (def ks-version "1.3.0")
-(def ps-version "2.4.0-master-SNAPSHOT")
+(def tk-version "1.3.1")
 
-(defn deploy-info
-  [url]
-  { :url url
-    :username :env/nexus_jenkins_username
-    :password :env/nexus_jenkins_password
-    :sign-releases false })
+(defproject puppetlabs/jruby-utils "0.1.0-SNAPSHOT"
+  :description "A library for working with JRuby"
+  :url "https://github.com/puppetlabs/jruby-utils"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
-(defproject puppetlabs/puppetserver ps-version
-  :description "Puppet Server"
+  :pedantic? :abort
 
-  :dependencies [[org.clojure/clojure ~clj-version]
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  :test-paths ["test/unit" "test/integration"]
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
 
                  ;; begin version conflict resolution dependencies
-                 [puppetlabs/typesafe-config "0.1.5"]
-                 [org.clojure/tools.macro "0.1.5"]
-                 [com.fasterxml.jackson.core/jackson-core "2.5.4"]
-                 [org.clojure/tools.reader "1.0.0-alpha1"]
                  ;; end version conflict resolution dependencies
-
-                 [cheshire "5.3.1"]
-                 [slingshot "0.10.3"]
-                 [clj-yaml "0.4.0" :exclusions [org.yaml/snakeyaml]]
-                 [commons-lang "2.6"]
-                 [commons-io "2.4"]
-                 [clj-time "0.11.0"]
-                 [prismatic/schema "1.0.4"]
-                 [me.raynes/fs "1.4.6"]
-                 [liberator "0.12.0"]
-                 [org.apache.commons/commons-exec "1.3"]
 
                  [org.jruby/jruby-core "1.7.20.1"
                   :exclusions [com.github.jnr/jffi com.github.jnr/jnr-x86asm]]
                  ;; jffi and jnr-x86asm are explicit dependencies because,
-                 ;; in JRuby's poms, they are defined using version ranges,
+                 ;; icn JRuby's poms, they are defined using version ranges,
                  ;; and :pedantic? :abort won't tolerate this.
                  [com.github.jnr/jffi "1.2.9"]
                  [com.github.jnr/jffi "1.2.9" :classifier "native"]
@@ -47,49 +31,19 @@
                  ;; 'uberjar-exclusions' example toward the end of this file.
                  [org.jruby/jruby-stdlib "1.7.20.1"]
 
-                 ;; we do not currently use this dependency directly, but
-                 ;; we have documentation that shows how users can use it to
-                 ;; send their logs to logstash, so we include it in the jar.
-                 ;; we may use it directly in the future
-                 [net.logstash.logback/logstash-logback-encoder "4.5.1"]
+
+                 [org.clojure/tools.logging "0.3.1"]
+                 [me.raynes/fs "1.4.6"]
+                 [prismatic/schema "1.0.4"]
 
 
                  [puppetlabs/trapperkeeper ~tk-version]
-                 [puppetlabs/trapperkeeper-authorization "0.5.0"]
-                 [puppetlabs/kitchensink ~ks-version]
-                 [puppetlabs/ssl-utils "0.8.1"]
-                 [puppetlabs/dujour-version-check "0.1.2" :exclusions [org.clojure/tools.logging]]
-                 [puppetlabs/http-client "0.5.0"]
-                 [puppetlabs/comidi "0.3.1"]]
+                 [puppetlabs/kitchensink ~ks-version]]
 
-  :main puppetlabs.trapperkeeper.main
-
-  :pedantic? :abort
-
-  :source-paths ["src/clj"]
-  :java-source-paths ["src/java"]
-  :test-paths ["test/unit" "test/integration"]
-  :resource-paths ["resources" "src/ruby"]
-
-  :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
-                 ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
-
-  :plugins [[lein-release "1.0.5" :exclusions [org.clojure/clojure]]]
-
-  :uberjar-name "puppet-server-release.jar"
-  :lein-ezbake {:vars {:user "puppet"
-                       :group "puppet"
-                       :build-type "foss"
-                       :java-args "-Xms2g -Xmx2g -XX:MaxPermSize=256m"
-                       :repo-target "PC1"}
-                :resources {:dir "tmp/ezbake-resources"}
-                :config-dir "ezbake/config"}
-
-  :lein-release {:scm         :git
-                 :deploy-via  :lein-deploy}
-
-  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
-                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]]
 
   ;; By declaring a classifier here and a corresponding profile below we'll get an additional jar
   ;; during `lein jar` that has all the code in the test/ directory. Downstream projects can then
@@ -97,58 +51,7 @@
   ;; code that we have.
   :classifiers [["test" :testutils]]
 
-  :profiles {:dev {:source-paths  ["dev"]
-                   :dependencies  [[org.clojure/tools.namespace "0.2.5"]
-                                   [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]
-                                   [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version :classifier "test"]
-                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]
-                                   [puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test"]
-                                   [ring-basic-authentication "1.0.5"]
-                                   [ring-mock "0.1.5"]
-                                   [spyscope "0.1.4" :exclusions [clj-time]]
-                                   [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
-                                   [beckon "0.1.1"]]
-                   :injections    [(require 'spyscope.core)]
-                   ; SERVER-332, enable SSLv3 for unit tests that exercise SSLv3
-                   :jvm-opts      ["-Djava.security.properties=./dev-resources/java.security"]}
-
-             :testutils {:source-paths ^:replace ["test/unit" "test/integration"]}
-
-             :ezbake {:dependencies ^:replace [;; we need to explicitly specify the clojure version
-                                               ;; here, because without it, lein brings in its own
-                                               ;; version, and older versions of lein (such as version
-                                               ;; 2.5.1 that is used on our jenkins servers at the time
-                                               ;; of this writing) depend on clojure 1.6.
-                                               [org.clojure/clojure ~clj-version]
-                                               [puppetlabs/puppetserver ~ps-version]
-                                               [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]
-                                               [org.clojure/tools.nrepl "0.2.3"]]
-                      :plugins [[puppetlabs/lein-ezbake "0.3.23"]]
-                      :name "puppetserver"}
-             :uberjar {:aot [puppetlabs.trapperkeeper.main]
-                       :dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty-version]]}
-             :ci {:plugins [[lein-pprint "1.1.1"]]}
-             :voom {:plugins [[lein-voom "0.1.0-20150115_230705-gd96d771" :exclusions [org.clojure/clojure]]]}}
-
-  :test-selectors {:integration :integration
-                   :unit (complement :integration)}
-
-  :aliases {"gem" ["trampoline" "run" "-m" "puppetlabs.puppetserver.cli.gem"]
-            "ruby" ["trampoline" "run" "-m" "puppetlabs.puppetserver.cli.ruby"]
-            "irb" ["trampoline" "run" "-m" "puppetlabs.puppetserver.cli.irb"]}
-
-  ; tests use a lot of PermGen (jruby instances)
-  :jvm-opts ["-XX:MaxPermSize=256m" "-Xmx2g"]
-
-  :repl-options {:init-ns user}
-
-  ;; NOTE: jruby-stdlib packages some unexpected things inside
-  ;; of its jar.  e.g., it puts a pre-built copy of the bouncycastle
-  ;; jar into its META-INF directory.  This is highly undesirable
-  ;; for projects that already have a dependency on a different
-  ;; version of bouncycastle.  Therefore, when building uberjars,
-  ;; you should take care to exclude the things that you don't want
-  ;; in your final jar.  Here is an example of how you could exclude
-  ;; that from the final uberjar:
-  :uberjar-exclusions [#"META-INF/jruby.home/lib/ruby/shared/org/bouncycastle"]
+  :profiles {:dev {:dependencies  [[puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test"]
+                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]]}
+             :testutils {:source-paths ^:replace ["test/unit" "test/integration"]}}
   )
