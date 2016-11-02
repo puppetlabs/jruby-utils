feefac290bcafe96c7f341712e79b1062bcc31a0
Jeremy Barlow <jeremy.barlow@puppet.com>
2016-11-02 13:14:35 -0700

    (SERVER-797) Remove `callMethod` methods from ScriptingContainer
    This commit removes all of the `callMethod` methods from the
    ScriptingContainer interface.  This is done since we have no known
    downstream consumers for these, pending some related changes in Puppet
    Server to switch over to `callMethodWithArgArray` instead.

--- src/java/com/puppetlabs/jruby_utils/jruby/ScriptingContainer.java
+++ src/java/com/puppetlabs/jruby_utils/jruby/ScriptingContainer.java
@@ -7,22 +7,6 @@ import org.jruby.runtime.Block;
 /**
  */
 public interface ScriptingContainer extends EmbedRubyInstanceConfigAdapter {
-    Object callMethod(Object receiver, String methodName, Object... args);
-    Object callMethod(Object receiver, String methodName,
-                      Block block, Object... args);
-    <T> T callMethod(Object receiver, String methodName, Class<T> returnType);
-    <T> T callMethod(Object receiver, String methodName, Object singleArg,
-                     Class<T> returnType);
-    <T> T callMethod(Object receiver, String methodName, Object[] args,
-                     Class<T> returnType);
-    <T> T callMethod(Object receiver, String methodName, Object[] args,
-                     Block block, Class<T> returnType);
-    <T> T callMethod(Object receiver, String methodName, Class<T> returnType,
-                     EmbedEvalUnit unit);
-    <T> T callMethod(Object receiver, String methodName, Object[] args,
-                     Class<T> returnType, EmbedEvalUnit unit);
-    <T> T callMethod(Object receiver, String methodName, Object[] args,
-                     Block block, Class<T> returnType, EmbedEvalUnit unit);
     Object callMethodWithArgArray(Object receiver,
                               String methodName,
                               Object[] args,
