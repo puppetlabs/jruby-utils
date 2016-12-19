f9a54c6caaf39a0d5c5f5c032a898a9f7dafabd9
Jenkins CI <ci@puppetlabs.com>
2016-12-19 11:08:20 -0800

    Version 0.6.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/jruby-utils "0.6.0"
+(defproject puppetlabs/jruby-utils "0.6.1-SNAPSHOT"
   :description "A library for working with JRuby"
   :url "https://github.com/puppetlabs/jruby-utils"
   :license {:name "Apache License, Version 2.0"
