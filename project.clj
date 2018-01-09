d64a692f85be24d232e9af1142bd7b93c2799d33
Justin Stoller <justin.stoller@gmail.com>
2018-01-09 15:39:48 -0800

    (SERVER-2060) Prep 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/jruby-utils "0.11.3-SNAPSHOT"
+(defproject puppetlabs/jruby-utils "1.0.0-SNAPSHOT"
   :description "A library for working with JRuby"
   :url "https://github.com/puppetlabs/jruby-utils"
   :license {:name "Apache License, Version 2.0"
