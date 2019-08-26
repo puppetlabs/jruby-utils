6b0f2315424ee68b8edc81030244e9e33856e5a7
Maggie Dreyer <maggie@puppet.com>
2019-08-26 10:51:36 -0700

    (SERVER-2611) Update JRuby to 9.2.8.0

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                  [slingshot]
 
                  [org.yaml/snakeyaml "1.23"]
-                 [puppetlabs/jruby-deps "9.2.7.0-1"]
+                 [puppetlabs/jruby-deps "9.2.8.0-1"]
 
                  [puppetlabs/i18n]
                  [puppetlabs/kitchensink]
