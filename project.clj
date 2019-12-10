aa3bc0d108034ba5d38c0e395fc66e7909458c45
Jenkins CI <ci@puppet.com>
2019-12-10 23:35:57 +0000

    Version 3.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/jruby-utils "3.0.0-SNAPSHOT"
+(defproject puppetlabs/jruby-utils "3.0.0"
   :description "A library for working with JRuby"
   :url "https://github.com/puppetlabs/jruby-utils"
   :license {:name "Apache License, Version 2.0"
