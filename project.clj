3d718e7154a49209c32745c5aec06a892442bfa7
donoghuc <cas.donoghue@gmail.com>
2023-05-03 11:34:03 -0700

    (PE-34960) Bump the major version of this project
    Given the new jruby version contains breaking changes due to basically being a jump from ruby 2 to ruby 3, bump the major version for this project.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/jruby-utils "4.0.4-SNAPSHOT"
+(defproject puppetlabs/jruby-utils "5.0.0-SNAPSHOT"
   :description "A library for working with JRuby"
   :url "https://github.com/puppetlabs/jruby-utils"
   :license {:name "Apache License, Version 2.0"
